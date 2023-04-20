4ea147474f98dccc11d25f731b89e4001273c5b1
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-04-21 00:27:32 +0530

    Delete Split_join.js

--- Split_join.js
+++
@@ -1,7 +0,0 @@
-const text = "Ram is a YouTube channel for JavaScript Coders";
-
-console.log(  text.split('o').length-1)
-    
-console.log( text.split( ' ' ).length) // split  
-
-console.log( text.split(' ').join( )) // join 
