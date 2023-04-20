9e9e72f43f5c172b4c73855642207aec5c7f4335
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-04-21 00:27:44 +0530

    Delete Splice.js

--- Splice.js
+++
@@ -1,8 +0,0 @@
-// Splice is use to cut the array element 
-
-const arr = [ 'a','b','c','d'];
-
-arr.splice(1,1,'x');   // in this row i have first one minning is chuse the array element number 
-                    // and second number one is cut elements in the  array and third is a replese and add this element 
-
-console.log(arr); // mining is print this array  
