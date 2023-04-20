a5cd62336f65ef4c53ee1b941f2ae77f0b88f6b4
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-04-21 00:23:08 +0530

    split_join in js

---
+++ JavaScript/Split_join.js
@@ -0,0 +1,7 @@
+const text = "Ram is a YouTube channel for JavaScript Coders";
+
+console.log(  text.split('o').length-1)
+    
+console.log( text.split( ' ' ).length) // split  
+
+console.log( text.split(' ').join( )) // join 
