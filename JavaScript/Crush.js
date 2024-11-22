b4adc034e363e9217fc27a97efd78625d56f2f2e
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2024-11-22 20:02:06 +0530

    Update Crush.js

--- JavaScript/Crush.js
+++ JavaScript/Crush.js
@@ -1,3 +1,22 @@
+/* Css code */
+/*
+#crush{
+    margin-top: 120px;
+    text-align: center;
+    justify-content: center;
+    font-size: 40px;
+    color:rgb(177, 9, 9);
+    text-shadow: 50px;
+}
+*/
+
+/* HTML Code */
+/*
+ <div  id="crush">
+        <script src="./t.js"></script>
+    </div>
+*/
+
 for (var row=0; row<=5; row++){
     for (var col=0; col<=6; col++){
         if((( col%3!=0) && (row==0)) || (( col%3==0)&& (row==1))){
