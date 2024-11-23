0243663ea50b9106c96409380074962d4a7e1767
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2024-11-23 17:02:54 +0530

    Update Crush.js

--- JavaScript/Crush.js
+++ JavaScript/Crush.js
@@ -1,19 +1,40 @@
 /* Css code */
-/*
-#crush{
+/* #crush{
     margin-top: 120px;
     text-align: center;
     justify-content: center;
     font-size: 40px;
     color:rgb(177, 9, 9);
     text-shadow: 50px;
+
 }
-*/
+button{
+    text-align: center;
+    justify-content: center;
+    font-size: bold 18px;
+    padding: 15px;
+    margin-top: 12px;
+    background-color: brown;
+    color: white;
+    border-radius: 20px;
+    font-family: cursive;
+}
+button:hover{
+cursor: pointer;
+background-color: white;
+color: brown;
+padding: 18px;
+margin-top: 12px;
+border-radius: 20px;
+font-family: cursive;
+font-size: 20px;
+} */
 
 /* HTML Code */
 /*
  <div  id="crush">
         <script src="./t.js"></script>
+        <button> Love You </button>
     </div>
 */
 
